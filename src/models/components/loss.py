54c3172e6419f7ccac2820e2b850850da2597f86
Alina Bazarova <bazarova1@jwlogin23.juwels>
2023-09-25 18:48:43 +0200

    SigClip Loss

--- src/models/components/loss.py
+++ src/models/components/loss.py
@@ -10,6 +10,95 @@ try:
 except ImportError:
     has_distributed = False
 
+
+def neighbour_exchange(from_rank, to_rank, tensor, group=None):
+    tensor_recv = torch.zeros_like(tensor)
+    send_op = torch.distributed.P2POp(
+        torch.distributed.isend,
+        tensor,
+        to_rank,
+        group=group,
+    )
+    recv_op = torch.distributed.P2POp(
+        torch.distributed.irecv,
+        tensor_recv,
+        from_rank,
+        group=group,
+    )
+    reqs = torch.distributed.batch_isend_irecv([send_op, recv_op])
+    for req in reqs:
+        req.wait()
+    return tensor_recv
+
+
+def neighbour_exchange_bidir(left_rank, right_rank, tensor_to_left, tensor_to_right, group=None):
+    tensor_from_left = torch.zeros_like(tensor_to_right)
+    tensor_from_right = torch.zeros_like(tensor_to_left)
+    send_op_left = torch.distributed.P2POp(
+        torch.distributed.isend,
+        tensor_to_left,
+        left_rank,
+        group=group,
+    )
+    send_op_right = torch.distributed.P2POp(
+        torch.distributed.isend,
+        tensor_to_right,
+        right_rank,
+        group=group,
+    )
+    recv_op_left = torch.distributed.P2POp(
+        torch.distributed.irecv,
+        tensor_from_left,
+        left_rank,
+        group=group,
+    )
+    recv_op_right = torch.distributed.P2POp(
+        torch.distributed.irecv,
+        tensor_from_right,
+        right_rank,
+        group=group,
+    )
+    reqs = torch.distributed.batch_isend_irecv([send_op_right, send_op_left, recv_op_right, recv_op_left])
+    for req in reqs:
+        req.wait()
+    return tensor_from_right, tensor_from_left
+
+
+
+class NeighbourExchange(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, from_rank, to_rank, group, tensor):
+        ctx.group = group
+        ctx.from_rank = from_rank
+        ctx.to_rank = to_rank
+        return neighbour_exchange(from_rank, to_rank, tensor, group=group)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return (None, None, None) + (NeighbourExchange.apply(ctx.to_rank, ctx.from_rank, ctx.group, grad_output),)
+
+
+def neighbour_exchange_with_grad(from_rank, to_rank, tensor, group=None):
+    return NeighbourExchange.apply(from_rank, to_rank, group, tensor)
+
+class NeighbourExchangeBidir(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, left_rank, right_rank, group, tensor_to_left, tensor_to_right):
+        ctx.group = group
+        ctx.left_rank = left_rank
+        ctx.right_rank = right_rank
+        return neighbour_exchange_bidir(left_rank, right_rank, tensor_to_left, tensor_to_right, group=group)
+
+    @staticmethod
+    def backward(ctx, *grad_outputs):
+        return (None, None, None) + \
+            NeighbourExchangeBidir.apply(ctx.right_rank, ctx.left_rank, ctx.group, *grad_outputs)
+
+
+
+def neighbour_exchange_bidir_with_grad(left_rank, right_rank, tensor_to_left, tensor_to_right, group=None):
+    return NeighbourExchangeBidir.apply(left_rank, right_rank, group, tensor_to_left, tensor_to_right)
+
 def gather_features(
         modality_features,
         sequence_features,
@@ -140,6 +229,118 @@ class ClipLoss(nn.Module):
             ) / 2
 
         return {"contrastive_loss": total_loss} if output_dict else total_loss
+
+
+
+class SigClipLoss(nn.Module):
+    """ Sigmoid Loss for Language Image Pre-Training (SigLIP) - https://arxiv.org/abs/2303.15343
+
+    @article{zhai2023sigmoid,
+      title={Sigmoid loss for language image pre-training},
+      author={Zhai, Xiaohua and Mustafa, Basil and Kolesnikov, Alexander and Beyer, Lucas},
+      journal={arXiv preprint arXiv:2303.15343},
+      year={2023}
+    }
+    """
+    def __init__(
+            self,
+            cache_labels=False,
+            rank=0,
+            world_size=1,
+            bidir=True,
+            use_ring_reduce=False,
+    ):
+        super().__init__()
+        self.cache_labels = cache_labels
+        self.rank = rank
+        self.world_size = world_size
+        #assert not use_horovod  # FIXME need to look at hvd ops for ring transfers
+        self.use_ring_reduce = use_ring_reduce
+        self.bidir = bidir
+
+        # cache state FIXME cache not currently used, worthwhile?
+        self.prev_num_logits = 0
+        self.labels = {}
+
+    def get_ground_truth(self, device, dtype, num_logits, negative_only=False) -> torch.Tensor:
+        labels = -torch.ones((num_logits, num_logits), device=device, dtype=dtype)
+        if not negative_only:
+            labels = 2 * torch.eye(num_logits, device=device, dtype=dtype) + labels
+        return labels
+
+    def get_logits(self, modality_features, sequence_features, logit_scale, logit_bias=None):
+        logits = logit_scale * modality_features @ sequence_features.T
+        if logit_bias is not None:
+            logits += logit_bias
+        return logits
+
+    def _loss(self, modality_features, sequence_features, logit_scale, logit_bias=None, negative_only=False):
+
+        logits = self.get_logits(modality_features, sequence_features, logit_scale, logit_bias)
+        labels = self.get_ground_truth(
+                modality_features.device,
+                modality_features.dtype,
+                modality_features.shape[0],
+                negative_only=negative_only,
+        )
+        loss = -F.logsigmoid(labels * logits).sum() / modality_features.shape[0]
+        return loss
+
+    def forward(self, modality_features, sequence_features, logit_scale=1.0, logit_bias=None, output_dict=False):
+        loss = self._loss(modality_features, sequence_features, logit_scale, logit_bias)
+
+        if self.world_size > 1:
+            # exchange text features w/ neighbour world_size - 1 times
+            right_rank = (self.rank + 1) % self.world_size
+            left_rank = (self.rank - 1 + self.world_size) % self.world_size
+            if self.bidir:
+                sequence_features_to_right = sequence_features_to_left = sequence_features
+                num_bidir, remainder = divmod(self.world_size - 1, 2)
+                for i in range(num_bidir):
+                    sequence_features_recv = neighbour_exchange_bidir_with_grad(
+                        left_rank,
+                        right_rank,
+                        sequence_features_to_left,
+                        sequence_features_to_right,
+                    )
+
+                    for f in sequence_features_recv:
+                        loss += self._loss(
+                            modality_features,
+                            f,
+                            logit_scale,
+                            logit_bias,
+                            negative_only=True,
+                        )
+                    sequence_features_to_left, sequence_features_to_right = sequence_features_recv
+
+                if remainder:
+                    sequence_features_recv = neighbour_exchange_with_grad(
+                        left_rank, right_rank, sequence_features_to_right)
+
+                    loss += self._loss(
+                        modality_features,
+                        sequence_features_recv,
+                        logit_scale,
+                        logit_bias,
+                        negative_only=True,
+                    )
+            else:
+                sequence_features_to_right = sequence_features
+                for i in range(self.world_size - 1):
+                    sequence_features_from_left = neighbour_exchange_with_grad(
+                        left_rank, right_rank, sequence_features_to_right)
+
+                    loss += self._loss(
+                        modality_features,
+                        sequence_features_from_left,
+                        logit_scale,
+                        logit_bias,
+                        negative_only=True,
+                    )
+                    sequence_features_to_right = sequence_features_from_left
+
+        return {"contrastive_loss": loss} if output_dict else loss
 
         
         
