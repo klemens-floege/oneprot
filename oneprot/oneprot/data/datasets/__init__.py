ccb0c38581d350205ff0b7b952ba1f67ca673288
klemens-floege <klemens@priorlabs.ai>
2025-08-08 20:39:28 +0200

    package structure

--- oneprot/oneprot/data/datasets/__init__.py
+++
@@ -1,5 +0,0 @@
-from .msa_dataset import MSADataset
-from .seqsim_dataset import SequenceSimDataset
-from .struct_graph_dataset import StructDataset
-from .struct_token_dataset import StructTokenDataset
-from .text_dataset import TextDataset
